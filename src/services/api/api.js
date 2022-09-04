d192879818b0c2c1cb89c0303a0fa1ca87c8a874
Ivan-Loboda <87610760+Ivan-Loboda@users.noreply.github.com>
2022-09-04 14:38:00 +0300

    update link

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const baseUrl = process.env.BASE_URL;
 
-axios.defaults.baseURL = 'http://localhost:3000';
-
+axios.defaults.baseURL = 'https://loboda-test-deploy.onrender.com';
+console.log(baseUrl);
 export const getDebtsList = () => axios.get('/api/debts');
 
 export const addNewDebt = (body) => axios.post('/api/debts/create', body);
